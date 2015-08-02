7be91e9113db1d665e048178fcecc600aa145b4a
jancbeck <jancbeck@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-02 22:58:11 +0000

    Release 1.0.1, see readme.txt for changelog.
    git-svn-id: https://plugins.svn.wordpress.org/force-delete-plugins/trunk@1211486 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ force-delete-plugins.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin Name: Force Delete Plugins
+Plugin URI:  https://wordpress.org/plugins/force-delete-plugins/
+Description: Changes the default behavior of the bulk delete plugin actions so it deletes plugins regardless whether they are active or not. Helpful for site developers.
+Version:     1.0.1
+Author:      Jan Beck
+Author URI:  http://jancbeck.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: force-delete-plugins
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+add_action( 'admin_init', function(){
+
+	if ( ! current_user_can('activate_plugins') )
+		wp_die(__('You do not have sufficient permissions to deactivate plugins for this site.'));
+
+	$wp_list_table = _get_list_table('WP_Plugins_List_Table');
+
+	$action = $wp_list_table->current_action();
+
+	if ( 'delete-selected' !== $action ) {
+		return;
+	}
+
+	check_admin_referer('bulk-plugins');
+
+	$plugins = isset( $_POST['checked'] ) ? (array) $_POST['checked'] : array();
+
+	deactivate_plugins( $plugins, false, is_network_admin() );
+
+} );
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Force Delete Plugins ===
+Contributors: jancbeck
+Tags: administration, plugin management, development, tools
+Requires at least: 4.2
+Tested up to: 4.2
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Changes the default behavior of the bulk delete plugin actions so it deletes plugins regardless whether they are active or not.
+
+== Installation ==
+
+To install the plugin and get it working:
+
+1. Upload plugin files to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Select plugins in the plugin table that you want to delete just like you would do normally.
+
+== Frequently Asked Questions ==
+
+= Why does this plugin exist?
+I was annoyed by the default behavior of WordPress and wanted to fix it. It's also an excuse to publish a WP plugin in the .org Repo.
+
+== Changelog ==
+
+= 1.0 =
+* Initial plugin version
